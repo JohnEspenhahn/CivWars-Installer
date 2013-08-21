62797aa28dae9cdaf02240a962d1df657454a118
JohnEspenhahn <john@espenhahn.org>
2013-08-21 12:37:26 -0400

    Libarary version 1

--- src/cpw/mods/fml/installer/VersionInfo.java
+++ src/cpw/mods/fml/installer/VersionInfo.java
@@ -1,125 +1,132 @@
-/*     */ package cpw.mods.fml.installer;
-/*     */ 
-/*     */ import argo.jdom.JdomParser;
-/*     */ import argo.jdom.JsonNode;
-/*     */ import argo.jdom.JsonRootNode;
-/*     */ import com.google.common.base.Charsets;
-/*     */ import com.google.common.base.Splitter;
-/*     */ import com.google.common.base.Throwables;
-/*     */ import com.google.common.collect.Iterables;
-/*     */ import com.google.common.io.ByteStreams;
-/*     */ import com.google.common.io.Files;
-/*     */ import com.google.common.io.OutputSupplier;
-/*     */ import java.io.File;
-/*     */ import java.io.IOException;
-/*     */ import java.io.InputStream;
-/*     */ import java.io.InputStreamReader;
-/*     */ 
-/*     */ public class VersionInfo
-/*     */ {
-/*  24 */   public static final VersionInfo INSTANCE = new VersionInfo();
-/*     */   public final JsonRootNode versionData;
-/*     */ 
-/*     */   public VersionInfo()
-/*     */   {
-/*  29 */     InputStream installProfile = getClass().getResourceAsStream("/install_profile.json");
-/*  30 */     JdomParser parser = new JdomParser();
-/*     */     try
-/*     */     {
-/*  34 */       this.versionData = parser.parse(new InputStreamReader(installProfile, Charsets.UTF_8));
-/*     */     }
-/*     */     catch (Exception e)
-/*     */     {
-/*  38 */       throw Throwables.propagate(e);
-/*     */     }
-/*     */   }
-/*     */ 
-/*     */   public static String getProfileName()
-/*     */   {
-/*  44 */     return INSTANCE.versionData.getStringValue(new Object[] { "install", "profileName" });
-/*     */   }
-/*     */ 
-/*     */   public static String getVersionTarget()
-/*     */   {
-/*  49 */     return INSTANCE.versionData.getStringValue(new Object[] { "versionInfo", "id" });
-/*     */   }
-/*     */ 
-/*     */   public static File getLibraryPath(File root) {
-/*  53 */     String path = INSTANCE.versionData.getStringValue(new Object[] { "install", "path" });
-/*  54 */     String[] split = (String[])Iterables.toArray(Splitter.on(':').omitEmptyStrings().split(path), String.class);
-/*  55 */     File dest = root;
-/*  56 */     Iterable<String> subSplit = Splitter.on('.').omitEmptyStrings().split(split[0]);
-/*  57 */     for (String part : subSplit)
-/*     */     {
-/*  59 */       dest = new File(dest, part);
-/*     */     }
-/*  61 */     dest = new File(new File(dest, split[1]), split[2]);
-/*  62 */     String fileName = split[1] + "-" + split[2] + ".jar";
-/*  63 */     return new File(dest, fileName);
-/*     */   }
-/*     */ 
-/*     */   public static String getVersion()
-/*     */   {
-/*  68 */     return INSTANCE.versionData.getStringValue(new Object[] { "install", "version" });
-/*     */   }
-/*     */ 
-/*     */   public static String getWelcomeMessage()
-/*     */   {
-/*  73 */     return INSTANCE.versionData.getStringValue(new Object[] { "install", "welcome" });
-/*     */   }
-/*     */ 
-/*     */   public static String getLogoFileName()
-/*     */   {
-/*  78 */     return INSTANCE.versionData.getStringValue(new Object[] { "install", "logo" });
-/*     */   }
-/*     */ 
-/*     */   public static JsonNode getVersionInfo()
-/*     */   {
-/*  83 */     return INSTANCE.versionData.getNode(new Object[] { "versionInfo" });
-/*     */   }
-/*     */ 
-/*     */   public static File getMinecraftFile(File path)
-/*     */   {
-/*  88 */     return new File(new File(path, getMinecraftVersion()), getMinecraftVersion() + ".jar");
-/*     */   }
-/*     */ 
-/*     */   public static String getContainedFile() {
-/*  92 */     return INSTANCE.versionData.getStringValue(new Object[] { "install", "filePath" });
-/*     */   }
+package cpw.mods.fml.installer;
 
-			public static String getCivWarsFile() {
-			  return INSTANCE.versionData.getStringValue(new Object[] { "install", "civwarsFile" });
-			}
-/*     */ 
-/*     */   public static void extractFile(File path) throws IOException {
-/*  96 */     INSTANCE.doFileExtract(path);
-/*     */   }
-	
-			public static void extractCivWarsFile(File path) throws IOException {
-			  INSTANCE.doCivWarsExtract(path);
-			}
-/*     */ 
-/*     */   private void doFileExtract(File path) throws IOException
-/*     */   {
-/* 101 */     InputStream inputStream = getClass().getResourceAsStream("/" + getContainedFile());
-/* 102 */     OutputSupplier outputSupplier = Files.newOutputStreamSupplier(path);
-/* 103 */     ByteStreams.copy(inputStream, outputSupplier);
-/*     */   }
+import argo.jdom.JdomParser;
+import argo.jdom.JsonNode;
+import argo.jdom.JsonRootNode;
+import com.google.common.base.Charsets;
+import com.google.common.base.Splitter;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Iterables;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.Files;
+import com.google.common.io.OutputSupplier;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-			public void doCivWarsExtract(File path) throws IOException
-			{
-			  InputStream inputStream = getClass().getResourceAsStream("/" + getCivWarsFile());
-			  OutputSupplier outputSupplier = Files.newOutputStreamSupplier(new File(path, getCivWarsFile()));
-			  ByteStreams.copy(inputStream, outputSupplier);
-			}
-/*     */ 
-/*     */   public static String getMinecraftVersion()
-/*     */   {
-/* 108 */     return INSTANCE.versionData.getStringValue(new Object[] { "install", "minecraft" });
-/*     */   }
-/*     */ }
+public class VersionInfo {
+    public static final VersionInfo INSTANCE = new VersionInfo();
+    public final JsonRootNode versionData;
 
-/* Location:           C:\Users\User\Desktop\minecraftforge-installer-1.6.2-9.10.0.804.jar
- * Qualified Name:     cpw.mods.fml.installer.VersionInfo
- * JD-Core Version:    0.6.2
+    public VersionInfo() {
+	InputStream installProfile = getClass().getResourceAsStream("/install_profile.json");
+	JdomParser parser = new JdomParser();
+	try {
+	    this.versionData = parser.parse(new InputStreamReader(installProfile, Charsets.UTF_8));
+	} catch (Exception e) {
+	    throw Throwables.propagate(e);
+	}
+    }
+
+    public static String getProfileName() {
+	return INSTANCE.versionData.getStringValue(new Object[] { "install", "profileName" });
+    }
+
+    public static String getVersionTarget() {
+	return INSTANCE.versionData.getStringValue(new Object[] { "versionInfo", "id" });
+    }
+
+    public static File getLibraryPath(File root) {
+	String path = INSTANCE.versionData.getStringValue(new Object[] { "install", "path" });
+	String[] split = (String[]) Iterables.toArray(Splitter.on(':').omitEmptyStrings().split(path), String.class);
+	File dest = root;
+	Iterable<String> subSplit = Splitter.on('.').omitEmptyStrings().split(split[0]);
+	for (String part : subSplit) {
+	    dest = new File(dest, part);
+	}
+	dest = new File(new File(dest, split[1]), split[2]);
+	String fileName = split[1] + "-" + split[2] + ".jar";
+	return new File(dest, fileName);
+    }
+
+    public static String getVersion() {
+	return INSTANCE.versionData.getStringValue(new Object[] { "install", "version" });
+    }
+
+    public static String getWelcomeMessage() {
+	return INSTANCE.versionData.getStringValue(new Object[] { "install", "welcome" });
+    }
+
+    public static String getLogoFileName() {
+	return INSTANCE.versionData.getStringValue(new Object[] { "install", "logo" });
+    }
+
+    public static JsonNode getVersionInfo() {
+	return INSTANCE.versionData.getNode(new Object[] { "versionInfo" });
+    }
+
+    public static File getMinecraftFile(File path) {
+	return new File(new File(path, getMinecraftVersion()), getMinecraftVersion() + ".jar");
+    }
+
+    public static String getContainedFile() {
+	return INSTANCE.versionData.getStringValue(new Object[] { "install", "filePath" });
+    }
+    
+    public static String getHahnAPIFile() {
+	return INSTANCE.versionData.getStringValue(new Object[] { "install", "hahnAPIFile" });
+    }
+
+    public static String getCivWarsFile() {
+	return INSTANCE.versionData.getStringValue(new Object[] { "install", "civwarsFile" });
+    }
+    
+    public static String getCivWarsTweakerFile() {
+	return INSTANCE.versionData.getStringValue(new Object[] { "install", "civwarsTweakerFile" });
+    }
+    
+    public static File getCivWarsLibraryPath(File root) {
+   	String path = INSTANCE.versionData.getStringValue(new Object[] { "install", "civwarsTweakerPath" });
+   	String[] split = (String[]) Iterables.toArray(Splitter.on(':').omitEmptyStrings().split(path), String.class);
+   	File dest = root;
+   	Iterable<String> subSplit = Splitter.on('.').omitEmptyStrings().split(split[0]);
+   	for (String part : subSplit) {
+   	    dest = new File(dest, part);
+   	}
+   	dest = new File(new File(dest, split[1]), split[2]);
+   	String fileName = split[1] + "-" + split[2] + ".jar";
+   	return new File(dest, fileName);
+   }
+
+    public static void extractFile(File path) throws IOException {
+	INSTANCE.doFileExtract(getContainedFile(), path);
+    }
+    
+    public static void extractHahnAPIFile(File path) throws IOException {
+	INSTANCE.doFileExtract(getHahnAPIFile(), path);
+    }
+    
+    public static void extractCivWarsFile(File path) throws IOException {
+	INSTANCE.doFileExtract(getCivWarsFile(), path);
+    }
+
+    public static void extractCivWarsTweakerFile(File path) throws IOException {
+	INSTANCE.doFileExtract(getCivWarsTweakerFile(), path);
+    }
+
+    private void doFileExtract(String src, File path) throws IOException {
+	InputStream inputStream = getClass().getResourceAsStream("/" + src);
+	OutputSupplier outputSupplier = Files.newOutputStreamSupplier(path);
+	ByteStreams.copy(inputStream, outputSupplier);
+    }
+
+    public static String getMinecraftVersion() {
+	return INSTANCE.versionData.getStringValue(new Object[] { "install", "minecraft" });
+    }
+}
+
+/*
+ * Location: C:\Users\User\Desktop\minecraftforge-installer-1.6.2-9.10.0.804.jar
+ * Qualified Name: cpw.mods.fml.installer.VersionInfo JD-Core Version: 0.6.2
  */
