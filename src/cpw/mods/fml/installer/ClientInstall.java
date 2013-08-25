9b6a4a01d86d9e97a6992b228eeb2fd9c3c7c0f9
JohnEspenhahn <john@espenhahn.org>
2013-08-25 09:22:40 -0400

    v1.4.0 install

--- src/cpw/mods/fml/installer/ClientInstall.java
+++ src/cpw/mods/fml/installer/ClientInstall.java
@@ -18,131 +18,158 @@ import javax.swing.JOptionPane;
 
 public class ClientInstall implements ActionType {
     public boolean run(File target) {
-	if (!target.exists()) {
-	    JOptionPane.showMessageDialog(null, "There is no minecraft installation at this location!", "Error", 0);
-	    return false;
-	}
-	File launcherProfiles = new File(target, "launcher_profiles.json");
-	if (!launcherProfiles.exists()) {
-	    JOptionPane.showMessageDialog(null, "There is no minecraft launcher profile at this location, you need to run the launcher first!", "Error", 0);
-	    return false;
-	}
-
-	File versionRootDir = new File(target, "versions");
-	File versionTarget = new File(versionRootDir, VersionInfo.getVersionTarget());
-	if ((!versionTarget.mkdirs()) && (!versionTarget.isDirectory())) {
-	    if (!versionTarget.delete()) {
-		JOptionPane.showMessageDialog(null, "There was a problem with the launcher version data. You will need to clear " + versionTarget.getAbsolutePath() + " manually", "Error", 0);
-	    } else {
-		versionTarget.mkdirs();
-	    }
-	}
-
-	File versionJsonFile = new File(versionTarget, VersionInfo.getVersionTarget() + ".json");
-	File versionJarFile = new File(versionTarget, VersionInfo.getVersionTarget() + ".jar");
-	File minecraftJarFile = VersionInfo.getMinecraftFile(versionRootDir);
-	try {
-	    Files.copy(minecraftJarFile, versionJarFile);
-	} catch (IOException e1) {
-	    JOptionPane.showMessageDialog(null, "You need to run the version " + VersionInfo.getMinecraftVersion() + " manually at least once", "Error", 0);
-	    return false;
-	}
-
-	File targetLibraryFile = VersionInfo.getLibraryPath(new File(target, "libraries"));
-	if ((!targetLibraryFile.getParentFile().mkdirs()) && (!targetLibraryFile.getParentFile().isDirectory())) {
-	    if (!targetLibraryFile.getParentFile().delete()) {
-		JOptionPane.showMessageDialog(null, "There was a problem with the launcher version data. You will need to clear " + targetLibraryFile.getAbsolutePath() + " manually", "Error", 0);
-		return false;
-	    }
-
-	    targetLibraryFile.getParentFile().mkdirs();
-	}
-
-	JsonRootNode versionJson = JsonNodeFactories.object(VersionInfo.getVersionInfo().getFields());
-	try {
-	    BufferedWriter newWriter = Files.newWriter(versionJsonFile, Charsets.UTF_8);
-	    PrettyJsonFormatter.fieldOrderPreservingPrettyJsonFormatter().format(versionJson, newWriter);
-	    newWriter.close();
-	} catch (Exception e) {
-	    JOptionPane.showMessageDialog(null, "There was a problem writing the launcher version data,  is it write protected?", "Error", 0);
-	    return false;
-	}
-
-	try {
-	    VersionInfo.extractFile(targetLibraryFile);
-	} catch (IOException e) {
-	    JOptionPane.showMessageDialog(null, "There was a problem writing the system library file", "Error", 0);
-	    return false;
-	}
-
-	JsonRootNode jsonProfileData;
-	JdomParser parser = new JdomParser();
-	try {
-	    jsonProfileData = parser.parse(Files.newReader(launcherProfiles, Charsets.UTF_8));
-	} catch (InvalidSyntaxException e) {
-	    JOptionPane.showMessageDialog(null, "The launcher profile file is corrupted. Re-run the minecraft launcher to fix it!", "Error", 0);
-	    return false;
-	} catch (Exception e) {
-	    throw Throwables.propagate(e);
-	}
-	
-	File civwarsFolder = new File(System.getProperty("user.home"), ".civwars");
-	File civwarsMods = new File(civwarsFolder, "mods");
-	civwarsMods.delete();
-	civwarsMods.mkdirs();
-	
-	File civwarsFile = new File(civwarsMods, VersionInfo.getCivWarsFile());
-	try {
-	    VersionInfo.extractCivWarsFile(civwarsFile);
-	} catch (Exception e) {
-	    JOptionPane.showMessageDialog(null, "There was a problem extracting Civwars into the Civwars folder.", "Error", 0);
-	    return false;
-	}
-
-	JsonField[] fields = { 
-		JsonNodeFactories.field("name", JsonNodeFactories.string(VersionInfo.getProfileName())), 
-		JsonNodeFactories.field("lastVersionId", JsonNodeFactories.string(VersionInfo.getVersionTarget())),
-		JsonNodeFactories.field("gameDir", JsonNodeFactories.string(civwarsFolder.getAbsolutePath()))
-	};
-
-	HashMap profileCopy = Maps.newHashMap(jsonProfileData.getNode(new Object[] { "profiles" }).getFields());
-	HashMap rootCopy = Maps.newHashMap(jsonProfileData.getFields());
-	profileCopy.put(JsonNodeFactories.string(VersionInfo.getProfileName()), JsonNodeFactories.object(fields));
-	JsonRootNode profileJsonCopy = JsonNodeFactories.object(profileCopy);
-
-	rootCopy.put(JsonNodeFactories.string("profiles"), profileJsonCopy);
-
-	jsonProfileData = JsonNodeFactories.object(rootCopy);
-	try {
-	    BufferedWriter newWriter = Files.newWriter(launcherProfiles, Charsets.UTF_8);
-	    PrettyJsonFormatter.fieldOrderPreservingPrettyJsonFormatter().format(jsonProfileData, newWriter);
-	    newWriter.close();
-	} catch (Exception e) {
-	    JOptionPane.showMessageDialog(null, "There was a problem writing the launch profile,  is it write protected?", "Error", 0);
-	    return false;
-	}
-
-	return true;
+        if (!target.exists()) {
+            JOptionPane.showMessageDialog(null, "There is no minecraft installation at this location!", "Error", 0);
+            return false;
+        }
+        File launcherProfiles = new File(target, "launcher_profiles.json");
+        if (!launcherProfiles.exists()) {
+            JOptionPane.showMessageDialog(null, "There is no minecraft launcher profile at this location, you need to run the launcher first!", "Error", 0);
+            return false;
+        }
+        
+        File versionRootDir = new File(target, "versions");
+        File versionTarget = new File(versionRootDir, VersionInfo.getVersionTarget());
+        if ((!versionTarget.mkdirs()) && (!versionTarget.isDirectory())) {
+            if (!versionTarget.delete()) {
+                JOptionPane.showMessageDialog(null, "There was a problem with the launcher version data. You will need to clear " + versionTarget.getAbsolutePath() + " manually", "Error", 0);
+            } else {
+                versionTarget.mkdirs();
+            }
+        }
+        
+        File versionJsonFile = new File(versionTarget, VersionInfo.getVersionTarget() + ".json");
+        File versionJarFile = new File(versionTarget, VersionInfo.getVersionTarget() + ".jar");
+        File minecraftJarFile = VersionInfo.getMinecraftFile(versionRootDir);
+        try {
+            Files.copy(minecraftJarFile, versionJarFile);
+        } catch (IOException e1) {
+            JOptionPane.showMessageDialog(null, "You need to run the version " + VersionInfo.getMinecraftVersion() + " manually at least once", "Error", 0);
+            return false;
+        }
+        
+        File targetLibraryFile = VersionInfo.getLibraryPath(new File(target, "libraries"));
+        if ((!targetLibraryFile.getParentFile().mkdirs()) && (!targetLibraryFile.getParentFile().isDirectory())) {
+            if (!targetLibraryFile.getParentFile().delete()) {
+                JOptionPane.showMessageDialog(null, "There was a problem with the launcher version data. You will need to clear " + targetLibraryFile.getAbsolutePath() + " manually", "Error", 0);
+                return false;
+            }
+            
+            targetLibraryFile.getParentFile().mkdirs();
+        }
+        
+        JsonRootNode versionJson = JsonNodeFactories.object(VersionInfo.getVersionInfo().getFields());
+        try {
+            BufferedWriter newWriter = Files.newWriter(versionJsonFile, Charsets.UTF_8);
+            PrettyJsonFormatter.fieldOrderPreservingPrettyJsonFormatter().format(versionJson, newWriter);
+            newWriter.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "There was a problem writing the launcher version data,  is it write protected?", "Error", 0);
+            return false;
+        }
+        
+        try {
+            VersionInfo.extractFile(targetLibraryFile);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "There was a problem writing the system library file", "Error", 0);
+            return false;
+        }
+        
+        JsonRootNode jsonProfileData;
+        JdomParser parser = new JdomParser();
+        try {
+            jsonProfileData = parser.parse(Files.newReader(launcherProfiles, Charsets.UTF_8));
+        } catch (InvalidSyntaxException e) {
+            JOptionPane.showMessageDialog(null, "The launcher profile file is corrupted. Re-run the minecraft launcher to fix it!", "Error", 0);
+            return false;
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+        
+        File civwarsFolder = new File(System.getProperty("user.home"), ".civwars");
+        
+        // Remove old files
+        for (String name : new String[] { "mods", "config" }) {
+            File civwarsFile = new File(civwarsFolder, name);
+            if (civwarsFile.exists()) {
+                if (!delete(civwarsFile)) {
+                    JOptionPane.showMessageDialog(null, "Failed to remove the old civwars folder '" + name + "'!", "Error", 0);
+                    return false;
+                }
+            }
+            civwarsFile.mkdirs();
+        }
+        
+        File civwarsFile = new File(new File(civwarsFolder, "mods"), VersionInfo.getCivWarsFile());
+        try {
+            VersionInfo.extractCivWarsFile(civwarsFile);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "There was a problem extracting Civwars into the Civwars folder.", "Error", 0);
+            return false;
+        }
+        
+        JsonField[] fields = {
+                JsonNodeFactories.field("name", JsonNodeFactories.string(VersionInfo.getProfileName())),
+                JsonNodeFactories.field("lastVersionId", JsonNodeFactories.string(VersionInfo.getVersionTarget())),
+                JsonNodeFactories.field("gameDir", JsonNodeFactories.string(civwarsFolder.getAbsolutePath())) };
+        
+        HashMap profileCopy = Maps.newHashMap(jsonProfileData.getNode(new Object[] { "profiles" }).getFields());
+        HashMap rootCopy = Maps.newHashMap(jsonProfileData.getFields());
+        profileCopy.put(JsonNodeFactories.string(VersionInfo.getProfileName()), JsonNodeFactories.object(fields));
+        JsonRootNode profileJsonCopy = JsonNodeFactories.object(profileCopy);
+        
+        rootCopy.put(JsonNodeFactories.string("profiles"), profileJsonCopy);
+        
+        jsonProfileData = JsonNodeFactories.object(rootCopy);
+        try {
+            BufferedWriter newWriter = Files.newWriter(launcherProfiles, Charsets.UTF_8);
+            PrettyJsonFormatter.fieldOrderPreservingPrettyJsonFormatter().format(jsonProfileData, newWriter);
+            newWriter.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "There was a problem writing the launch profile,  is it write protected?", "Error", 0);
+            return false;
+        }
+        
+        return true;
     }
-
+    
+    private boolean delete(File f) {
+        if (f.exists()) {
+            if (f.isDirectory()) {
+                for (File cF: f.listFiles()) {
+                    if (!delete(cF))
+                        return false;
+                }
+                
+                return f.delete();
+            } else {
+                return f.delete();
+            }
+        } else {
+            return true;
+        }
+    }
+    
     public boolean isPathValid(File targetDir) {
-	if (targetDir.exists()) {
-	    File launcherProfiles = new File(targetDir, "launcher_profiles.json");
-	    return launcherProfiles.exists();
-	}
-	return false;
+        if (targetDir.exists()) {
+            File launcherProfiles = new File(targetDir, "launcher_profiles.json");
+            return launcherProfiles.exists();
+        }
+        return false;
     }
-
+    
     public String getFileError(File targetDir) {
-	if (targetDir.exists()) {
-	    return "The directory is missing a launcher profile. Please run the minecraft launcher first";
-	}
-
-	return "There is no minecraft directory set up. Either choose an alternative, or run the minecraft launcher to create one";
+        if (targetDir.exists()) {
+            return "The directory is missing a launcher profile. Please run the minecraft launcher first";
+        }
+        
+        return "There is no minecraft directory set up. Either choose an alternative, or run the minecraft launcher to create one";
     }
-
+    
     public String getSuccessMessage() {
-	return String.format("Successfully installed client profile %s for version %s into launcher", new Object[] { VersionInfo.getProfileName(), VersionInfo.getVersion() });
+        return String.format("Successfully installed client profile %s for version %s into launcher", new Object[] {
+                VersionInfo.getProfileName(),
+                VersionInfo.getVersion() });
     }
 }
 
